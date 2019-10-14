43202380208e1e1029de39d96cd65d4ca64bfd31
Denis <dearodriguezve@unal.edu.co>
2019-10-13 21:43:19 -0500

    Recibiendo Inputs y usando metodos
    Se utilizo v-model @click y se implemento methods

--- script.js
+++ script.js
@@ -7,6 +7,15 @@ const app = new Vue({
             {nombre:'Pera', cantidad: 10},
             {nombre:'Manzana', cantidad: 0},
             {nombre:'Platano', cantidad: 13}
-        ]
+        ],
+        nuevaFruta:''
+    },
+    methods:{
+        agregarFruta: function(){
+            this.frutas2.push({
+                nombre:this.nuevaFruta,
+                cantidad: 0
+            })
+        }
     }
 })
